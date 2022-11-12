c8c60a0a1e7b7e943a9d4df1401238643904fda7
CollinM <collinmcneese@github.com>
2022-11-11 18:01:37 -0600

    dep bump, index redo to console out
    Signed-off-by: CollinM <collinmcneese@github.com>

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 // index.js
 // See README.md for requirements and usage instructions.
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+let msg = `
+This repository is made to be executed by a GitHub Actions workflow.
+See README.md for more details.
+`;
 
-app.get('/', (req, res) => {
-  const response = `
-  <html>
-    <center>
-    This repository is made to be executed by a GitHub Actions workflow. <br />
-    See README.md for more details.
-    </center>
-  </html>
-  `;
-  res.send(response);
-});
-
-app.listen(port, () => {
-  console.log('Listening on http://127.0.0.1:%s', port);
-});
+console.log(msg);
